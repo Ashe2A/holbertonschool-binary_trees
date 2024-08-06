f8981e170ec65fbf3318a3943d8ab637162bedcc
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-06 10:40:04 +0200

    0 - init

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_t - creates a binary tree node
+ * @parent: parent of the new node
+ * @value: value to inpue in the node
+ *
+ * Return: the new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t new_node = malloc(sizeof(binary_tree_t));
+
+    if (parent != NULL)
+    {
+        new_node->parent = parent;
+        new_node->left = NULL;
+        new_node->right = NULL;
+        if (parent->left != NULL)
+        {
+            parent->left = new_node;
+        }
+        else if (parent->right)
+        {
+            parent->right = new_node;
+        }
+        else
+        {
+            free(new_node);
+            return (NULL);
+        }
+        return (new_node);
+    }
+    free(new_node);
+    return (NULL);
+}
