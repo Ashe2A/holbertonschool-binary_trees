e4dd30d72d58ebe2f00ba94c915cc6878c793ed7
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-06 23:48:38 +0200

    0 - parent's attributes already take the return value

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -18,15 +18,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
         new_node->left = NULL;
         new_node->right = NULL;
         new_node->n = value;
-        if (parent->left == NULL)
-            parent->left = new_node;
-        else if (parent->right == NULL)
-            parent->right = new_node;
-        else
-        {
-            free(new_node);
-            return (NULL);
-        }
         return (new_node);
     }
     free(new_node);
