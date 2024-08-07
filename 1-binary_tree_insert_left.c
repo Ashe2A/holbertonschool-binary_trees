9a8d72f33bdda7a5aa22e05e03cd8fe47f0bac28
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-07 11:28:42 +0200

    1 - re-order nod overwriting

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,18 +11,20 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *old_node = parent->left;
 
 	if ((new_node == NULL) || (parent == NULL))
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->parent = parent;
-	new_node->left = parent->left;
-	if (parent->left != NULL)
-		parent->left->parent[0] = new_node[0];
-	new_node->right = NULL;
 	new_node->n = value;
 	parent->left = new_node;
+	new_node->parent = parent;
+	new_node->left = old_node;
+	if (old_node != NULL)
+		old_node->parent = new_node;
+	new_node->right = NULL;
+	
 	return (new_node);
 }
