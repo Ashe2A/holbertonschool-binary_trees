049025e931d344f169b46d6ec5e9adab237f0a0d
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-07 14:26:59 +0200

    1 - changing new_node->left doesn't change parent->left->parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,8 +10,7 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-	binary_tree_t *old_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = binary_tree_node(parent, value);
 
 	if ((new_node == NULL) || (parent == NULL))
 	{
@@ -19,16 +18,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		free(old_node);
 		return (NULL);
 	}
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
 	if (parent->left != NULL)
 	{
-		old_node = parent->left;
-		new_node->left = old_node;
-		old_node->parent = new_node;
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
 	}
 	parent->left = new_node;
-	new_node->parent = parent;
 	return (new_node);
 }
