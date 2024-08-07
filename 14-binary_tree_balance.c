f40b06f57b9fbbd22acf90cd5d75b4d85577518f
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-08 01:48:12 +0200

    14 - simple return (heights should work)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,19 +9,7 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int balance = 0;
-
 	if (tree != NULL)
-	{
-		if (tree->left != NULL)
-		{
-			balance += binary_tree_height(tree->left);
-		}
-		if (tree->right != NULL)
-		{
-			balance -= (binary_tree_height(tree->right) - 1);
-		}
-		return (balance);
-	}
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 	return (0);
 }
