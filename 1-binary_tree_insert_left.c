98e4692b691c59d0fb63c3e9a6cf97e408a38ec3
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-07 11:31:41 +0200

    1 - old node needs parent to not be null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,13 +11,14 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-	binary_tree_t *old_node = parent->left;
+	binary_tree_t *old_node = malloc(sizeof(binary_tree_t));
 
 	if ((new_node == NULL) || (parent == NULL))
 	{
 		free(new_node);
 		return (NULL);
 	}
+	old_node = parent->left;
 	new_node->n = value;
 	parent->left = new_node;
 	new_node->parent = parent;
@@ -25,6 +26,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (old_node != NULL)
 		old_node->parent = new_node;
 	new_node->right = NULL;
-	
+
 	return (new_node);
 }
