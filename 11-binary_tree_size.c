332f58b87bb461f847b08c91c39a8ea3d28ffe85
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-07 19:01:13 +0200

    11 - tree size count

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - get tree size
+ * @tree: the tree to count the nodes in
+ *
+ * Return: size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t tree_size = 0;
+
+	if ((tree != NULL))
+	{
+		if (tree->left != NULL)
+			tree_size += binary_tree_size(tree->left);
+		if (tree->right != NULL)
+			tree_size += binary_tree_size(tree->right);
+		return (tree_size);
+	}
+
+	return (0);
+}
