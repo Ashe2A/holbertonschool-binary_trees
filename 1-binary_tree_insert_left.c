13633fbc1737cbaf21ba09e1a84de8dcf0c853c9
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-07 14:54:57 +0200

    1 - old node doesn't exist anymore

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if ((new_node == NULL) || (parent == NULL))
 	{
 		free(new_node);
-		free(old_node);
 		return (NULL);
 	}
 	if (parent->left != NULL)
