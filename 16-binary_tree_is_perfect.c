c0a71f2cd4242f92850b88bdee4383ddac021153
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-08 13:39:40 +0200

    16 - left and right must have separate returns

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,15 +10,16 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int perfection = 1;
+	int left_perfection = 1;
+	int right_perfection = 1;
 
-	if (tree != NULL && (binary_tree_balance(tree) == 0) && (binary_tree_is_full(tree->left) == 1))
+	if ((tree != NULL) && (binary_tree_balance(tree) == 0) && (binary_tree_is_full(tree->left) == 1))
 	{
 		if (tree->left != NULL)
-			perfection *= binary_tree_is_perfect(tree->left);
+			left_perfection *= binary_tree_is_perfect(tree->left);
 		if (tree->right != NULL)
-			perfection *= binary_tree_is_perfect(tree->right);
-		return (perfection);
+			right_perfection *= binary_tree_is_perfect(tree->right);
+		return (left_perfection * right_perfection);
 	}
 	return (0);
 }
