1bd6d72e11abd99c2a4c2eb16780410ede871f10
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-08 15:20:46 +0200

    17 - node isn't a const (copy error)

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,7 +6,7 @@
  *
  * Return: the sibling node
  */
-binary_tree_t *binary_tree_sibling(const binary_tree_t *node)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if ((node != NULL) && (node->parent != NULL)
 	&& (node->parent->left != NULL) && (node->parent->right != NULL))
