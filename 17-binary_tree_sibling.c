2084c5d7709d923dda84af6f107642d58f5eaf32
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-08 15:21:36 +0200

    17 - right address call

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,9 +11,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if ((node != NULL) && (node->parent != NULL)
 	&& (node->parent->left != NULL) && (node->parent->right != NULL))
 	{
-		if ((&(node->parent->left)) == &node)
+		if ((*(node->parent->left)) == *node)
 			return (node->parent->right);
-		if ((&(node->parent->right)) == &node)
+		if ((*(node->parent->right)) == *node)
 			return (node->parent->left);
 	}
 	return (NULL);
