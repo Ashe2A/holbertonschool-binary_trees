4f8a836dc48e40cce6060c40f81cbaa9971e0c66
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-08 15:01:19 +0200

    16 - tree is full not left subtree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,12 +13,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int left_perfection = 1;
 	int right_perfection = 1;
 
-	if ((tree != NULL) && (binary_tree_balance(tree) == 0) && (binary_tree_is_full(tree->left) == 1))
+	if ((tree != NULL)
+	&& (binary_tree_balance(tree) == 0)
+	&& (binary_tree_is_full(tree) == 1))
 	{
 		if (tree->left != NULL)
-			left_perfection *= binary_tree_is_perfect(tree->left);
+			left_perfection = binary_tree_is_perfect(tree->left);
 		if (tree->right != NULL)
-			right_perfection *= binary_tree_is_perfect(tree->right);
+			right_perfection = binary_tree_is_perfect(tree->right);
 		return (left_perfection * right_perfection);
 	}
 	return (0);
