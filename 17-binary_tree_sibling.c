3c8a4a1c7e94e0eb498cc9d1b3e5d429a2109ff7
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-08 15:15:35 +0200

    17 - find the sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - node sibling
+ * @node: the tree to check
+ *
+ * Return: the sibling node
+ */
+binary_tree_t *binary_tree_sibling(const binary_tree_t *node)
+{
+	if ((node != NULL) && (node->parent != NULL)
+	&& (node->parent->left != NULL) && (node->parent->right != NULL))
+	{
+		if ((&(node->parent->left)) == &node)
+			return (node->parent->right);
+		if ((&(node->parent->right)) == &node)
+			return (node->parent->left);
+	}
+	return (NULL);
+}
